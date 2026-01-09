b0065e47697db79eee53330978a1e8b1e3920c4d
vino-01 <vinothinisenniappan1204@gmail.com>
2026-01-09 22:45:03 +0530

    Third commit

--- Student.java
+++ Student.java
@@ -1,12 +1,14 @@
 public class Student {
     int id;
     String name;
+    int number;
     int marks;
     char grade;
 
-    public Student(String name, int marks, char grade) {
+    public Student(String name, int marks, int number,char grade) {
         this.name = name;
         this.marks = marks;
+        this.number=number;
         this.grade = grade;
     }
 }
